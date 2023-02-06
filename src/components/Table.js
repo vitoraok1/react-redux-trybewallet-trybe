ae32fa9bc82be106f4ddca95fe67850c516e179a
vitoraok1 <vitoraoki17@gmail.com>
2023-02-06 08:57:28 -0300

    requisito 07

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import './Table.css';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -19,10 +22,74 @@ class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {
+              expenses && expenses.map((expense, index) => {
+                const {
+                  description, tag, method, value, exchangeRates, currency,
+                } = expense;
+                return (
+                  <tr key={ index }>
+                    <td>
+                      {description}
+                    </td>
+                    <td>
+                      {tag}
+                    </td>
+                    <td>
+                      {method}
+                    </td>
+                    <td>
+                      {parseFloat(value).toFixed(2)}
+                    </td>
+                    <td>
+                      {exchangeRates[currency].name}
+                    </td>
+                    <td>
+                      {parseFloat(exchangeRates[currency].ask).toFixed(2)}
+                    </td>
+                    <td>
+                      {parseFloat(exchangeRates[currency].ask
+                    * value).toFixed(2)}
+                    </td>
+                    <td>
+                      Real
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                      >
+                        Editar
+                      </button>
+                      <button
+                        type="button"
+                      >
+                        Excluir
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.string,
+    description: PropTypes.string,
+    currency: PropTypes.string,
+    method: PropTypes.string,
+    tag: PropTypes.string,
+  })).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
